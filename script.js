d60f3127f0fe934b552e9c2b3d172d0fa7e3ca1f
bnday01 <bnday01@msn.com>
2018-12-25 11:26:44 -0500

    2.0.0 Massive content, new features

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 //Non-commercial use only
 
 
-//First, we'll create global objects to avoid repeat fetches
+//First, we'll create global constants to avoid repeat fetches
+//Here's the master SWAPI object;
 const SWAPI = {
 	People:[],
 	Planets:[],
@@ -13,7 +14,7 @@ const SWAPI = {
 	Films:[]
 };
 
-//We'll use this to hold the values of the promises
+//We'll use this array to hold the values of the promises
 const SWAPIarr = [];
 
 //Let's grab the information from the SWAPI
@@ -38,7 +39,6 @@ const getSWAPI = async function(){
 			const response = await request.json();
 			//Targeting object clusters
 			const data = response.results;
-			
 
 			//Let's copy the response into our SWAPI arr
 			SWAPIarr.push(data);
@@ -50,146 +50,114 @@ const getSWAPI = async function(){
 			}
 	})));
 }
-//Let's go ahaead and call this function ^^^
+//Let's go ahead and call this function ^^^
 getSWAPI();
-
-
 
 //It has come to my attention that i'll need a lookup function to retrieve info that I need.
 //Here it is-
 
 const lookup = (link) => {
-	
-	  const regExp = /https:\/\/swapi\.co\/api\/(\w+)\/(\d+)\//;
-	  const match = link.match(regExp);
-	  const section = match[1].replace(match[1][0],match[1][0].toUpperCase());
+  
+  const regExp = /https:\/\/swapi\.co\/api\/(\w+)\/(\d+)\//;
+  const match = link.match(regExp);
+  const section = match[1].replace(match[1][0],match[1][0].toUpperCase());
 
-	  const results = () => {
-	  	return SWAPI[section].filter((obj) => obj.url == link);
-	  }
-	  return results()[0].name;
+  const results = () => {
+  	return SWAPI[section].filter((obj) => obj.url == link);
+  }
+  return results()[0].name;
 }
 
-//Lets start refining data!
+//Let's use Cardify to refine our data!
 //We'll start by grabbing the "container" from the DOM
-
 const container = document.querySelector('#container');
 
-//Now, let's create a function that creates HTML elements for our containter
+//You may have noticed an animation in the background;
+//I've used cardify to procedurally create them:
+const backgroundAnim = () =>{
 
-const newElement = (tag, attr, id,text) => {
-	const element = document.createElement(tag);
-	const textNode = document.createTextNode(text);
-	element.appendChild(textNode);
-	//Ternary statements to prevent 'undefined' attributes
-	(attr) ? element.className = attr : null;
-	(id) ? element.id = id : null;
-	return element;
+	const animation = (amount, distance, distanceRate, delay, delayRate) =>{
+	
+		 for (let i = 1; i <= amount; i++){
+		 	const rectangle = Card.div("anim-rectangleR", `rect${i}`);
+		 	rectangle.style['animation-delay'] = `${delay}s`;
+		 	rectangle.style.top = `${distance}px`;
+		 	delay += delayRate;
+		 	distance += distanceRate;
+		 	container.append(rectangle);
+
+		 }
+	}
+	return animation(16, 0, 40, 0, .25);
 }
+backgroundAnim();
 
-//We also need a function to add the elements 
+//We also need a function to add the main section elements 
 const addElements = (parent) => {
 	//We're going to define each new Element to be added to "container"
 	const define = {
-		sectionFilms:newElement("section","main",'Films' ,''),
-		sectionPlanets:newElement("section","main",'Planets' ,''),
-		sectionStarships:newElement("section","main",'Starships' ,''),
-		sectionVehicles:newElement("section","main",'Vehicles' , ''),
-		sectionPeople:newElement("section","main",'People' ,''),
-		sectionSpecies:newElement("section","main",'Species' ,'')
+		sectionFilms:Card.section("main",'Films' ,''),
+		sectionPlanets:Card.section("main",'Planets' ,''),
+		sectionStarships:Card.section("main",'Starships' ,''),
+		sectionVehicles:Card.section("main",'Vehicles' , ''),
+		sectionPeople:Card.section("main",'People' ,''),
+		sectionSpecies:Card.section("main",'Species' ,'')
 	}
 	//Now lets loop through the definition object to finish adding them;
 	//Also, throw in an h2 element linked to the main element's id
 	for (props in define){
 
-		define[props].appendChild(newElement("h2","","",define[props].id));
-		parent.appendChild(define[props]);
+		define[props].append(Card.h("2","","",define[props].id));
+		parent.append(define[props]);
 	}
 }
 //We'll go ahead and call this ^^
 addElements(container);
 
-//At this point we're going to need some type of card element to hold all this data nicely
 //Most Likely, we'll need to organize the cards differently in each section
 //That means we'll probally need a card for each section 
-
-//
+	
+	//Here's a looped function to create a few lists with our lookup data as text
+	//I'll probobly have to use this for the rest of the cards, so i'll make it a global function
+const divTrain = (h6text, listArray) => {
+	const div = Card.div();
+	const ul = Card.ul();
+	listArray.map(item => {
+		return ul.append(Card.li("","", lookup(item))); 
+	});
+	div.append(Card.h("6", "","", h6text));
+	div.append(ul);
+	return div;
+} 
 
 const cardFilms =  (obj) => {
 	
-	//Let's start by creating each element we need
-	//Note* For clarity, these elements have been created in the order they need to be appended
-	const base = newElement("div","card", `film${obj.episode_id}`,"");
-	const h4 = () => {
-		const h4text = `Episode ${obj.episode_id}: ${obj.title}`;
-		return newElement("h4", "", "", h4text);
-	}
-	const small = newElement("small","","", `${obj.release_date}`);
-	const img = () => { 
-		const elem = newElement("img","","","");
-		//Here's a placeholder for a future loop through image links
-		elem.currentSrc = "#";
-		elem.alt = "Poster";
-		return elem;
-	}
-	const pDirector = newElement("p","","",`Directed by ${obj.director}`);
-	const pProducer = newElement("p","","",`Produced by ${obj.producer}`);
-	const h5 = newElement("h5","","", "Featured");
-	//We have a few more elements, but some are double nested so let's do that now.
-	const article = newElement("article", "","","");
-	
-
-	const divTrain = (h6text, listArray) => {
-		const div = newElement("div","","","");
-		const h6 = newElement("h6", "","", h6text);
-		const ul = newElement("ul","","","");
-		listArray.map(item => {
-			return ul.appendChild(newElement("li","","", lookup(item))); 
-		});
-		div.appendChild(h6);
-		div.appendChild(ul);
-	
-		return div;
-	} 
+	//Let's start by creating a few constants so we dont have to chain appends
+	const base = Card.div("card", `film${obj.episode_id}`,"");
+	const article = Card.article();
 
 
-	const pCrawl = newElement("p","",`crawl${obj.episode_id}`, obj.opening_crawl);
+	base.append(Card.h("4","","",`Episode ${obj.episode_id}: ${obj.title}`));
+	base.append(Card.small(obj.release_date));
+	base.append(Card.img("#","Poster"));
+	base.append(Card.p("","",`Directed by ${obj.director}`));
+	base.append(Card.p("","",`Directed by ${obj.producer}`));
+	base.append(Card.h("5","","","Featured"));
+	base.append(Card.p("",`crawl${obj.episode_id}`, obj.opening_crawl));
+	article.append(divTrain("People", obj.characters));
+	article.append(divTrain("Planets", obj.planets));
+	article.append(divTrain("Vehicles", obj.vehicles));
+	article.append(divTrain("Species", obj.species));
+	article.append(divTrain("Starships", obj.starships));
+	base.append(article);
 	//And that's all for the basic film card.
-	//Time to put everything together and return one element.
-
-	base.appendChild(h4());
-	base.appendChild(small);
-	base.appendChild(img());
-	base.appendChild(pDirector);
-	base.appendChild(pProducer);
-	base.appendChild(h5);
-	base.appendChild(pCrawl);
-	article.appendChild(divTrain("People", obj.characters));
-	article.appendChild(divTrain("Planets", obj.planets));
-	article.appendChild(divTrain("Vehicles", obj.vehicles));
-	article.appendChild(divTrain("Species", obj.species));
-	article.appendChild(divTrain("Starships", obj.starships));
-	base.appendChild(article);
-
 	
 	return base;
 }
 
-//Now That we have a card, lets look at how iterate it.
-//At some point, I'll probobally have to iterate all of them. So for now, lets continue making cards.
-const buttons = {
-	filmsButton:document.querySelector('#bFilms'),
-	planetsButton:document.querySelector('#bPlanets'),
-	peopleButton:document.querySelector('#bPeople'),
-	speciesButton:document.querySelector('#bSpecies'),
-	vehiclesButton:document.querySelector('#bVehicles'),
-	starshipsButton:document.querySelector('#bStarships'),
-}
-
-const sectionGroup = {
+const cardPlanets = (obj) => {
 
 }
-
 
 
 setTimeout(function(){
@@ -223,32 +191,75 @@ setTimeout(function(){
 	//console.log(SWAPI);
 	//Congrats!!! We now have a cached version of SWAPI!
 
-
+	//Let's Iterate them now 
+	//Note* I could definitly apply DRY standards, but for the sake of readability this is much more clear and concise;
+	
+	//Films
 	const filmsArr = SWAPI.Films;
 	const filmsSection = document.querySelector('#Films');
 	for (i = 0; i < filmsArr.length; i++){
-	filmsSection.appendChild(cardFilms(filmsArr[i]));
+		filmsSection.append(cardFilms(filmsArr[i]));
 	}
 
+	//Planets
+	const planetsArr = SWAPI.Planets;
+	const planetsSection = document.querySelector('#Planets');
+	for (i = 0; i < planetsArr.length; i++){
+		planetsSection.append();
+	}
+
+	//Starships
+	const starshipsArr = SWAPI.Starships;
+	const starshipsSection = document.querySelector('#Starships');
+	for (i = 0; i < starshipsArr.length; i++){
+		starshipsSection.append();
+	}
+
+	//Vehicles
+	const vehiclesArr = SWAPI.Vehicles;
+	const vehiclesSection = document.querySelector('#Vehicles');
+	for (i = 0; i < vehiclesArr.length; i++){
+		vehiclesSection.append();
+	}
+
+	const peopleArr = SWAPI.People;
+	const peopleSection = document.querySelector('#People');
+	for (i = 0; i < peopleArr.length; i++){
+		peopleSection.append();
+	}
+
+	const speciesArr = SWAPI.Species;
+	const speciesSection = document.querySelector('#Species');
+	for (i = 0; i < speciesArr.length; i++){
+		speciesSection.append();
+	}
 }, 15000);
  
 
 
 
-	//Lets bind this to a click event for the films button
-	//In anticipation of multiple DOM lookups, ill create an object.
+	//Lets bind click events to the buttons
+	//In anticipation of multiple DOM lookups, i'll create an object.
+	const buttons = {
+		filmsButton:document.querySelector('#bFilms'),
+		planetsButton:document.querySelector('#bPlanets'),
+		peopleButton:document.querySelector('#bPeople'),
+		speciesButton:document.querySelector('#bSpecies'),
+		vehiclesButton:document.querySelector('#bVehicles'),
+		starshipsButton:document.querySelector('#bStarships')
+	}
 
-	function hide(){
-		const elem = this.id.replace(this.id[0],"");
-		const allSections = document.getElementsByTagName('section');
-		const section = document.getElementById(elem);
-		
-		for (var i = 0; i < allSections.length; i++) {
-		allSections[i].classList.add('hide');
-		}
-		section.classList.remove('hide');
+function hide(){
+	const elem = this.id.replace(this.id[0],"");
+	const allSections = document.getElementsByTagName('section');
+	const section = document.getElementById(elem);
+	
+	for (var i = 0; i < allSections.length; i++) {
+	allSections[i].classList.add('hide');
 	}
-	for (eachButton in buttons){
-		buttons[eachButton].addEventListener("click", hide);
-	}
+	section.classList.remove('hide');
+}
+for (eachButton in buttons){
+	buttons[eachButton].addEventListener("click", hide);
+}
 
--- scriptURLS.js
+++ scriptURLS.js
@@ -6,3 +6,5 @@ const urls = [
 ['https://swapi.co/api/starships/','https://swapi.co/api/starships/?page=2','https://swapi.co/api/starships/?page=3','https://swapi.co/api/starships/?page=4'],
 ['https://swapi.co/api/vehicles/','https://swapi.co/api/vehicles/?page=2','https://swapi.co/api/vehicles/?page=3','https://swapi.co/api/vehicles/?page=4'],
 ['https://swapi.co/api/species/','https://swapi.co/api/species/?page=2','https://swapi.co/api/species/?page=3','https://swapi.co/api/species/?page=4']]
+
+const images = ["https://static1.squarespace.com/static/570c331be321403a73df5d4e/t/57f3b79fc534a5db61a9e09c/1475590053137/?format=500w"];
